d7a2b4749df7006bc8bf2fea8433892f3b75de91
szczepanwikar@gmail.com <szczepanwikar@gmail.com>
2023-04-01 14:48:03 +0200

    configure services

--- Services/ChallangeService.cs
+++ Services/ChallangeService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,21 @@ namespace ImproveMe.Services
 {
     public class ChallangeService
     {
+        SQLiteAsyncConnection Database;
+        private readonly UserService _userService;
+        public ChallangeService(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        async Task Init()
+        {
+            if (Database is not null)
+                return;
+
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+            var result = await Database.CreateTableAsync<User>();
+        }
+
     }
 }
--- Services/UserService.cs
+++ Services/UserService.cs
@@ -19,12 +19,35 @@ namespace ImproveMe.Services
 
             Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
             var result = await Database.CreateTableAsync<User>();
+
+            var user = await GetUserAsync();
+            if(user is null)
+            {
+                await CreateUserAsync();
+            }
         }
 
         public async Task<User> GetUserAsync()
         {
             await Init();
             return await Database.Table<User>().FirstOrDefaultAsync();
+        }
+
+        public async Task<User> CreateUserAsync()
+        {
+            await Init();
+            var user = new User()
+            {
+                Name = "Jan",
+                LastName = "Kowalski",
+                Points = 0,
+                Level = 1,
+                LastLogged = new(),
+                Streak = 0
+            };
+
+            await Database.InsertAsync(user);
+            return await GetUserAsync();
         }
 
     }
